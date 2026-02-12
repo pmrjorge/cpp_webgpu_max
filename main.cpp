d12a5237b0b4561147d9ec8f71e8f3795414e0cf
Pedro Jorge <pmrjge@gmail.com>
2026-02-12 06:43:04 +0000

    Synced with the online tutorial code

--- main.cpp
+++ main.cpp
@@ -3,6 +3,23 @@
 
 int main() {
     WGPUInstanceDescriptor descriptor {};
+    descriptor.nextInChain = nullptr;
+
+#ifdef WEBGPU_BACKEND_EMSCRIPTEN
+    WGPUInstance instance = wgpuCreateInstance(nullptr);
+#else //  WEBGPU_BACKEND_EMSCRIPTEN
+    WGPUInstance instance = wgpuCreateInstance(&descriptor);
+#endif //  WEBGPU_BACKEND_EMSCRIPTEN
+
+    if (!instance) {
+        std::cerr << "Failed to create WGPU instance" << std::endl;
+        return 1;
+    }
+
+    std::cout << "WGPU instance: " << instance << std::endl;
+
+
+    wgpuInstanceRelease(instance);
 
     return 0;
 }
